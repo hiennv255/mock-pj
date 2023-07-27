be64d4319d50a1976553841d5ac7b1be8f85b7e1
hiennv05 <bg.hien@gmail.com>
2023-07-27 12:50:09 +0900

    Start pj

---
+++ Backend/src/main/java/com/vti/mockpj/MockPjApplication.java
@@ -0,0 +1,13 @@
+package com.vti.mockpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockPjApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/vti/mockpj/MockPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.mockpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
